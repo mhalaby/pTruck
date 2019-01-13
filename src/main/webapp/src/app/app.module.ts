44f4b676275ac867e787eeac120a9ebc6f6413c7
melhalaby <m.elhalaby@celonis.de>
2019-01-14 00:18:26 +0100

    adding angular material

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -7,7 +7,22 @@ import { TruckViewComponent } from './truck/view/details/truckView.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TruckCreateComponent } from './truck/view/create-vew/create-view.component';
 import { TruckListComponent } from './truck/view/truck-list/truck-list.component';
-
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatCardModule,
+  MatListModule,
+  MatSidenavModule,
+  MatExpansionModule,
+  MatToolbarModule,
+  MatFormFieldModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +34,22 @@ import { TruckListComponent } from './truck/view/truck-list/truck-list.component
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    MatMenuModule,
+    ReactiveFormsModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule
 
   ],
   providers: [],
